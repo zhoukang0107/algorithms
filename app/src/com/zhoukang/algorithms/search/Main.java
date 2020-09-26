d9f3de3c51d4796c614f299654d12306580824fb
ZACK-PC\Zack <11123180416@qq.com>
2020-09-27 01:15:08 +0800

    tree

--- app/src/com/zhoukang/algorithms/search/Main.java
+++ app/src/com/zhoukang/algorithms/search/Main.java
@@ -3,7 +3,17 @@ package com.zhoukang.algorithms.search;
 public class Main {
 
     public static void main(String[] args){
-
+        int[] arr = new int[]{0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9};
+        System.out.println(bSearch(arr, 4));
+        System.out.println(bSearchFirst(arr, 4));
+        System.out.println(bSearchLast(arr, 4));
+        System.out.println(bSearchFirstBiger(arr, 4));
+        System.out.println(bSearchFirstSmaller(arr, 4));
+        System.out.println(bSearchFirstBigerAndEqual(arr, 4));
+        System.out.println(bSearchLastSmallerAndEqual(arr, 4));
+        int[] arr2 = new int[]{0,1,2,3,4,5,6,7,8,9};
+        System.out.println(bSearch(arr2, 4));
+        System.out.println(bSearch(arr2, 0, arr.length-1,4));
     }
 
     public static int bSearch(int[] arr, int value){
@@ -41,13 +51,13 @@ public class Main {
         int low = 0;
         int high = arr.length-1;
         while (low<=high){
-            int mind = low + (high-low)>>1;
+            int mind = low + (high-low)/2;
             if (arr[mind] > value){
                 high = mind-1;
             } else if (arr[mind] < value){
                 low = mind + 1;
             } else {
-                if (mind == 0 || arr[mind-1] == value) return mind;
+                if (mind == 0 || value > arr[mind-1]) return mind;
                 high = mind-1;
             }
         }
@@ -66,7 +76,7 @@ public class Main {
             } else if (arr[mind] < value){
                 low = mind + 1;
             } else {
-                if (mind == arr.length-1 || arr[mind+1] == value) return mind;
+                if (mind == arr.length-1 || arr[mind+1] > value) return mind;
                 high = mind+1;
             }
         }
@@ -78,9 +88,11 @@ public class Main {
         int low = 0;
         int high = arr.length-1;
         while (low<=high){
-            int mind = low + (high-low)>>1;
-            if (arr[mind] >= value){
-                if (mind==0||arr[mind-1])
+            int mind = low + (high-low)/2;
+            if (arr[mind] > value){
+                if (mind == 0 ||arr[mind-1]==value) {
+                    return high;
+                }
                 high = mind-1;
             } else {
                 low = mind + 1;
@@ -93,20 +105,46 @@ public class Main {
         int low = 0;
         int high = arr.length-1;
         while (low<=high){
-            int mind = low + (high-low)>>1;
-            if (arr[mind] > value){
+            int mind = low + (high-low)/2;
+            if (arr[mind] >= value){
                 high = mind-1;
-            } else if (arr[mind] < value){
-                low = mind + 1;
             } else {
-                if (mind == arr.length-1 || arr[mind+1] == value) return mind;
-                high = mind+1;
+                if (mind == 0 || arr[mind+1] >= value) return mind;
+                low = mind + 1;
             }
         }
         return -1;
     }
 
     //变体三：查找第一个大于等于给定值的元素
+    public static int bSearchFirstBigerAndEqual(int[] arr, int value){
+        int low = 0;
+        int heigh = arr.length - 1;
+        while (low<=heigh){
+            int mind = low + (heigh - low)/2;
+            if (value<=arr[mind]){
+                if (mind==0|| arr[mind-1]<value) return mind;
+                heigh = mind - 1;
+            } else {
+                low = mind +1;
+            }
+        }
+        return -1;
+    }
 
     //变体四：查找最后一个小于等于给定值的元素
+    public static int bSearchLastSmallerAndEqual(int[] arr, int value){
+        int low = 0;
+        int heigh = arr.length - 1;
+        while (low <= heigh){
+            int mind = low + (heigh-low)/2;
+            if (value > arr[mind]){
+                low = mind +1;
+            } else {
+                if (mind == 0|| arr[mind-1] <value) return mind;
+                heigh = mind - 1;
+            }
+        }
+        return -1;
+    }
 }
--- app/src/com/zhoukang/algorithms/tree/Main.java
+++ app/src/com/zhoukang/algorithms/tree/Main.java
@@ -1,9 +1,485 @@
 package com.zhoukang.algorithms.tree;
 
 
+import java.util.*;
 
 public class Main {
+    /**
+     *       1
+     *   2       5
+     * 3   4   6   7
+     *
+     * @param args
+     */
+    public static void main(String[] args){
+        TreeNode root = new TreeNode();
+        root.value = 1;
+        root.left = new TreeNode();
+        root.left.value = 2;
+        root.left.left = new TreeNode();
+        root.left.left.value = 3;
+        root.left.right = new TreeNode();
+        root.left.right.value = 4;
 
+        root.right = new TreeNode();
+        root.right.value = 5;
+        root.right.left = new TreeNode();
+        root.right.left.value = 6;
+        root.right.right = new TreeNode();
+        root.right.right.value = 7;
+
+        List<TreeNode> list = new ArrayList<>();
+        preTraversal(root, list);
+        System.out.println(list);
+        System.out.println(preTraversal(root));
+        System.out.println(preTraversal1(root));
+        List<TreeNode> list1 = new ArrayList<>();
+        preTraversal1(root, list1);
+        System.out.println(list1);
+        List<TreeNode> list2 = new ArrayList<>();
+        preTraversal2(root, list2);
+        System.out.println(list2);
+        System.out.println("************************");
+        List<TreeNode> list3 = new ArrayList<>();
+        inTraversal(root, list3);
+        System.out.println(list3);
+        List<TreeNode> list4 = new ArrayList<>();
+        inTraversal1(root, list4);
+        System.out.println(list4);
+        System.out.println("************************");
+        List<TreeNode> list5 = new ArrayList<>();
+        postTraversal(root, list5);
+        System.out.println(list5);
+        List<TreeNode> list6 = new ArrayList<>();
+        postTraversal1(root, list6);
+        System.out.println(list6);
+        List<TreeNode> list7 = new ArrayList<>();
+        postTraversal2(root, list7);
+        System.out.println(list7);
+        System.out.println("************************");
+        System.out.println("层序遍历");
+        List<TreeNode> list8 = new ArrayList<>();
+        bfs(root, list8);
+        System.out.println(list8);
+        List<TreeNode> list9 = new ArrayList<>();
+        bfs1(root, list9);
+        System.out.println(list9);
+        System.out.println("深度优先遍历");
+        List<TreeNode> list10 = new ArrayList<>();
+        dfs(root, list10, new HashSet<>());
+        System.out.println(list10);
+        System.out.println("最大深度计算");
+        System.out.println(getTreeMaxLevel(root));
+        System.out.println(getTreeMaxLevel1(root));
+        System.out.println("最小深度计算");
+        System.out.println(getTreeMinLevel(root));
+        System.out.println(getTreeMinLevel1(root));
+
+    }
+
+    /**
+     * 前序遍历
+     * @param node
+     * @param list
+     */
+    public static void preTraversal(TreeNode node, List<TreeNode> list){
+        list.add(node);
+        if (node.left != null) preTraversal(node.left, list);
+        if (node.right != null) preTraversal(node.right, list);
+    }
+
+    public static  List<TreeNode> preTraversal1(TreeNode node){
+        List<TreeNode> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (node != null || !stack.isEmpty()){
+            if (node != null){
+                stack.push(node);
+                list.add(node);
+                node = node.left;
+            } else {
+                node = stack.pop().right;
+            }
+        }
+        return list;
+    }
+
+    public static  List<TreeNode> preTraversal(TreeNode node){
+        List<TreeNode> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (node != null || !stack.isEmpty()){
+            if (node != null){
+                stack.push(node);
+                list.add(node);
+            }
+            //左结点不为空
+            if (node != null && node.left != null){
+                node = node.left;
+            } else if (node != null && node.right!=null){ //左结点为空的话，访问右结点
+                node = node.right;
+            } else {
+                //node的左右结点都为空，该结点出栈
+                node = stack.pop().right;
+            }
+        }
+        return list;
+    }
+
+    /**
+     * 前序遍历 非递归
+     * @param node
+     * @param list
+     */
+    public static void preTraversal1(TreeNode root, List<TreeNode> list){
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        while (node!=null ){
+            stack.push(node);
+            list.add(node);
+            if (node.left != null){
+                node = node.left;
+            } else if (node.right != null){
+                node = node.right;
+            } else {
+                do {
+                    node = stack.pop().right;
+                } while (!stack.isEmpty()&&node == null);
+            }
+        }
+    }
+
+    public static void preTraversal2(TreeNode root, List<TreeNode> list){
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        while (node!=null || !stack.isEmpty() ){
+            if (node != null){
+                stack.push(node);
+                list.add(node);
+            }
+
+            if (node != null && node.left != null){
+                node = node.left;
+            } else if (node != null && node.right != null){
+                node = node.right;
+            } else {
+                node = stack.pop().right;
+            }
+        }
+    }
+
+    public static void inTraversal(TreeNode root, List<TreeNode> list){
+        if (root == null){
+            return;
+        }
+        if (root.left != null) inTraversal(root.left, list);
+        list.add(root);
+        if (root.right != null) inTraversal(root.right, list);
+    }
+
+    public static void inTraversal1(TreeNode root, List<TreeNode> list){
+        if (root == null){
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        while (node!= null || !stack.isEmpty()){
+            if (node!=null){
+                stack.push(node);
+            }
+            if (node != null && node.left != null){
+                node = node.left;
+            } else if (node != null && node.right != null){
+                node = node.right;
+            } else {
+                list.add(stack.pop());
+                node = null;
+                while (!stack.isEmpty()){
+                    TreeNode n = stack.pop();
+                    list.add(n);
+                    if (n.right!= null){
+                        node = n.right;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void postTraversal(TreeNode root, List<TreeNode> list){
+        if (root == null){
+            return;
+        }
+        if (root.left != null) postTraversal(root.left, list);
+        if (root.right != null) postTraversal(root.right, list);
+        list.add(root);
+    }
+
+
+    public static void postTraversal1(TreeNode root, List<TreeNode> list){
+        if (root == null){
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        TreeNode lastVisiabe = null;
+        while (!stack.isEmpty()|| node != null){
+            stack.push(node);
+
+            if (node.left != null &&(lastVisiabe == null || (node.left != lastVisiabe &&  node.right != lastVisiabe))){
+                node = node.left;
+            } else if (node.right != null &&(lastVisiabe == null || (node.left != lastVisiabe &&  node.right != lastVisiabe))){
+                node = node.right;
+            } else {
+                list.add(stack.pop());
+                lastVisiabe = node;
+
+                node = null;
+                while (!stack.isEmpty()){
+                    TreeNode n = stack.peek();
+                    if (n.right !=null && lastVisiabe != n.right){
+                        node = n.right;
+                        break;
+                    }
+                    lastVisiabe = stack.pop();
+                    list.add(lastVisiabe);
+                }
+            }
+        }
+    }
+
+    public static void postTraversal2(TreeNode root, List<TreeNode> list){
+        if (root == null){
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        Stack<TreeNode> out = new Stack<>();
+        TreeNode node = root;
+        while (!stack.isEmpty()|| node != null){
+            if (node != null){
+                stack.push(node);
+                out.push(node);
+            }
+            if (node != null&&node.right != null){
+                node = node.right;
+            } else if (node!=null && node.left!=null){
+                node = node.left;
+            } else {
+                node = stack.pop().left;
+            }
+        }
+        while (!out.isEmpty()){
+            list.add(out.pop());
+        }
+    }
+
+    /**
+     * 层序遍历、广度遍历
+     * @param root
+     * @param list
+     */
+    public static void bfs(TreeNode root, List<TreeNode> list){
+        if (root == null) return;
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            list.add(node);
+            if (node.left != null){
+                queue.offer(node.left);
+            }
+            if (node.right != null){
+                queue.offer(node.right);
+            }
+        }
+    }
+
+    public static void bfs1(TreeNode root, List<TreeNode> list){
+        if (root == null) return;
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int count = queue.size();
+            for (int i=0;i<count;i++){
+                TreeNode node = queue.poll();
+                list.add(node);
+                if (node.left != null){
+                    queue.offer(node.left);
+                }
+                if (node.right != null){
+                    queue.offer(node.right);
+                }
+            }
+        }
+    }
+
+    /**
+     * 深度优先遍历  前序遍历
+     * @param root
+     * @param list
+     * @param visited
+     */
+    public static void dfs(TreeNode root, List<TreeNode> list, Set<TreeNode> visited){
+        if (root == null) return;
+        visited.add(root);
+        list.add(root);
+        if (root.left != null && !visited.contains(root.left)){
+            dfs(root.left,list,visited);
+        }
+
+        if (root.right != null && !visited.contains(root.right)){
+            dfs(root.right,list,visited);
+        }
+    }
+
+    public static void dfs1(TreeNode root, List<TreeNode> list){
+        if (root == null) return;
+        Set<TreeNode> visited = new HashSet<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        while (!stack.isEmpty()){
+            if (node != null){
+                stack.push(root);
+                visited.add(root);
+            }
+            if (node.left != null && !visited.contains(node.left)){
+                node = node.left;
+            } else if (node.right != null){
+                node = node.right;
+            }
+        }
+    }
+
+    public static int getTreeMaxLevel(TreeNode node){
+        if (node == null) return 0;
+        return 1 + Math.max(getTreeMaxLevel(node.left),getTreeMaxLevel(node.right));
+    }
+
+    public static int getTreeMaxLevel1(TreeNode root){
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int heigh = 0;
+        while (!queue.isEmpty()){
+            heigh++;
+            int count = queue.size();
+            for (int i=0;i<count;i++){
+                TreeNode node = queue.poll();
+                if (node.left != null){
+                    queue.offer(node.left);
+                }
+                if (node.right != null){
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return heigh;
+    }
+
+    public static int getTreeMinLevel(TreeNode node){
+        if (node == null) return 0;
+        return 1 + Math.min(getTreeMinLevel(node.left),getTreeMinLevel(node.right));
+    }
+
+    public static int getTreeMinLevel1(TreeNode root){
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int heigh = 0;
+        while (!queue.isEmpty()){
+            heigh++;
+            int count = queue.size();
+            for (int i=0;i<count;i++){
+                TreeNode node = queue.poll();
+                if (node.left != null){
+                    queue.offer(node.left);
+                }
+                if (node.right != null){
+                    queue.offer(node.right);
+                }
+                if (node.left == null && node.right==null){
+                    return heigh;
+                }
+            }
+        }
+        return heigh;
+    }
+
+    public static boolean isBinarySearchTree(TreeNode node){
+        if (node == null) return true;
+        TreeNode left = getMaxLeft(node.left);
+        TreeNode right = getMinRight(node.right);
+        if (left !=null && left.value > node.value){
+            return false;
+        }
+        if (right !=null && right.value < node.value){
+            return false;
+        }
+        return isBinarySearchTree(node.left) && isBinarySearchTree(node.right);
+    }
+
+    private static TreeNode getMinRight(TreeNode node) {
+        if (node == null) return null;
+        while (node.left != null){
+            node = node.left;
+        }
+        return node;
+    }
+
+    private static TreeNode getMaxLeft(TreeNode node) {
+        if (node == null) return null;
+        while (node.right != null){
+            node = node.right;
+        }
+        return node;
+    }
+
+    public static boolean isBinarySearchTree1(TreeNode node){
+        List<TreeNode> list = new ArrayList<>();
+        inTraversal(node, list);
+        TreeNode pre = null;
+        for (TreeNode n: list){
+            if (pre == null){
+                pre = n;
+                continue;
+            }
+            if (pre.value<=n.value){
+                pre = n;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isBinarySearchTree2(TreeNode node){
+        TreeNode[] pre = new TreeNode[1];
+        return isBST(node, pre);
+    }
+
+    private static boolean isBST(TreeNode node, TreeNode[] pre) {
+        if (node == null){
+            return true;
+        }
+        if (!isBST(node.left, pre)){
+            return false;
+        }
+        if (pre[0] == null){
+            pre[0] = node;
+        } else {
+            if (pre[0].value <= node.value){
+                pre[0] = node;
+            } else {
+                return false;
+            }
+        }
+        if (!isBST(node.right, pre)){
+            return false;
+        }
+        return true;
+    }
 
     //求二叉树中结点p和q的第一个公共父节点
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
@@ -41,4 +517,11 @@ class TreeNode{
     int value;
     TreeNode left;
     TreeNode right;
+
+    @Override
+    public String toString() {
+        return "TreeNode{" +
+                "value=" + value +
+                '}';
+    }
 }
